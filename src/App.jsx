d4315128eab5cfc7d64709a32dbf223f0bc9fc3a
jungwoo <jw.roh629@gmail.com>
2022-08-28 00:02:22 +0900

    chore: tailwind css 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
 import './App.css'
-import Button from 'react-bootstrap/Button'
+import { Navbar, Container, Nav } from 'react-bootstrap';
 function App() {
 
   return (
     <div className="App">
-      <Button variant="primary">Primary</Button>
-
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div className="px-10">hihi</div>
     </div>
   )
 }
